3810eb013f9daa7de1a5133d068eba77e8d71e2a
linsgry <rytman@uji.es>
2024-02-18 19:35:41 +0100

    v3.2
    Corregido el movimiento
    Añadidos los rangos y normalizando ángulos

--- person_follower/person_follower.py
+++ person_follower/person_follower.py
@@ -38,9 +38,38 @@ class PersonFollower(Node):
         #
         # your code for computing vx, wz
         #
-        vx = 0.
-        wz = 0.
+        #vx = 0.
+        #wz = 0.
+
+        closest_range = min(ranges)
+        closest_index = ranges.index(closest_range)
+
         #
+        min_range = 0.2  
+        max_range = 2.0  
+        follow_distance = 1.0  
+        speed_factor = 0.5  # velocidad lineal vx
+        turn_factor = 1.0  # velocidad angular wz
+
+        
+        if closest_range > max_range or closest_range < min_range:
+            vx = 0.0  # si el objeto esta fuera del rango o demasiado cerca
+        else:
+            vx = max(0.0, min(speed_factor * (closest_range - follow_distance), speed_factor))
+
+        # posicion angular del punto más cercano
+        angle_to_closest = angle_min + closest_index * angle_increment
+
+        # Ajuste de la velocidad angular basado en la posición angular del punto más cercano
+        # Convertir ángulo a una dirección relativa (-1 a 1, donde 0 es directamente adelante)
+        if angle_to_closest > 0:
+            direction = (angle_to_closest - angle_min) / (angle_max - angle_min) * 2 - 1
+        else:
+            direction = (angle_to_closest - angle_max) / (angle_max - angle_min) * 2 + 1
+
+        wz = direction * turn_factor
+        #
+
         output_msg = Twist()
         output_msg.linear.x = vx
         output_msg.angular.z = wz
