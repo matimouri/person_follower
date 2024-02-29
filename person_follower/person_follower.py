39c7e79df436b787aa5c0fe63ca430b5891a1436
matimouri <150012971+matimouri@users.noreply.github.com>
2024-02-29 21:26:47 +0100

    Update Febrero

--- person_follower/person_follower.py
+++ person_follower/person_follower.py
@@ -30,50 +30,32 @@ class PersonFollower(Node):
             10)
         self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, input_msg):
-        angle_min = input_msg.angle_min
-        angle_max = input_msg.angle_max
-        angle_increment = input_msg.angle_increment
-        ranges = input_msg.ranges
-        #
-        # your code for computing vx, wz
-        #
-        #vx = 0.
-        #wz = 0.
+    def listener_callback(self, msg):
+        ranges = msg.ranges
+        detection_zone = ranges[144:215]
 
-        closest_range = min(ranges)
-        closest_index = ranges.index(closest_range)
+        vx = 0.0
+        wz = 0.0
 
-        #
-        min_range = 0.2  
-        max_range = 2.0  
-        follow_distance = 1.0  
-        speed_factor = 0.5  # velocidad lineal vx
-        turn_factor = 1.0  # velocidad angular wz
+        min_distance = 0.6
+        max_distance = 1.4
+        forward_speed = 0.2
+        rotation_speed = 0.3
+        slope_index = 26
 
-        
-        if closest_range > max_range or closest_range < min_range:
-            vx = 0.0  # si el objeto esta fuera del rango o demasiado cerca
-        else:
-            vx = max(0.0, min(speed_factor * (closest_range - follow_distance), speed_factor))
+        for i, distance in enumerate(detection_zone):
+            if min_distance < distance < max_distance:
+                vx = 0.06 + (distance / max_distance) * forward_speed / 2
+                if i < slope_index:
+                    wz = rotation_speed
+                elif i > len(detection_zone) - slope_index:
+                    wz = -rotation_speed
+                break
 
-        # posicion angular del punto más cercano
-        angle_to_closest = angle_min + closest_index * angle_increment
-
-        # Ajuste de la velocidad angular basado en la posición angular del punto más cercano
-        # Convertir ángulo a una dirección relativa (-1 a 1, donde 0 es directamente adelante)
-        if angle_to_closest > 0:
-            direction = (angle_to_closest - angle_min) / (angle_max - angle_min) * 2 - 1
-        else:
-            direction = (angle_to_closest - angle_max) / (angle_max - angle_min) * 2 + 1
-
-        wz = direction * turn_factor
-        #
-
-        output_msg = Twist()
-        output_msg.linear.x = vx
-        output_msg.angular.z = wz
-        self.publisher_.publish(output_msg)
+        velocity_msg = Twist()
+        velocity_msg.linear.x = vx
+        velocity_msg.angular.z = wz
+        self.publisher_.publish(velocity_msg)
 
 def main(args=None):
     rclpy.init(args=args)
